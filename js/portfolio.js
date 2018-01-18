007a50c94fd5766cbd84cda8f0b1357c618689e6
mkltkn <mkltkn@gmail.com>
2018-01-18 18:56:22 +0100

    Initial commit

---
+++ js/portfolio.js
@@ -0,0 +1,126 @@
+(function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 70)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+  
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+      $('.navbar-collapse').collapse('hide');
+    });
+  
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 80
+    });
+  
+    // Collapse Navbar
+    var navbarCollapse = function() {
+      if ($("#mainNav").offset().top > 100) {
+        $("#mainNav").addClass("navbar-shrink");
+      } else {
+        $("#mainNav").removeClass("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+  
+    // Modal popup$(function () {
+    $('.portfolio-item').magnificPopup({
+      type: 'inline',
+      preloader: false,
+      focus: '#username',
+      modal: true
+    });
+    $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+      e.preventDefault();
+      $.magnificPopup.close();
+    });
+  
+    // Floating label headings for the contact form
+    $(function() {
+      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+      }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+      });
+    });
+  
+  })(jQuery); // End of use strict
+
+  
+  // Ajax Portfolio Function
+
+function ajax_get(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log('responseText:' + xmlhttp.responseText);
+            try {
+                var data = JSON.parse(xmlhttp.responseText);
+            } catch(err) {
+                console.log(err.message + " in " + xmlhttp.responseText);
+                return;
+            }
+            callback(data);
+        }
+    };
+ 
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+ajax_get('./js/portfoliodata.json', function(data) {
+  document.getElementById('portfolio').innerHTML = data['portfolio'];
+ 
+  let html = ``;
+  //for in degerleri iterate etmek icin 
+  data['portfolio'].forEach(key => {
+    html += `
+      <div class="col-lg-6 col-lg-4">
+        <div class="card h-100"> 
+          <a href="${key['url']}" target="blank">
+            <img 
+              class="card-img-top" 
+              src="${key['imgthumb']}" 
+              alt="">
+          </a>
+        <div class="card-body"> 
+          <h4 class="card-title"> 
+            <a href="#">${key['title']}</a>
+          </h4>
+          <p class="card-text">
+            ${key['content']}
+          </p>
+        </div>
+      </div>
+    </div>`;
+  });
+    document.getElementById('portfolio').innerHTML = html;
+});
